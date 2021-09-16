acf9c211b088f6ab2da483e46e120f3b57ecffa2
Johannes <johannesjandke@gmail.com>
2021-09-16 18:03:13 +0200

    added class for "yellow" button support but not called in MainActivity

---
+++ app/src/main/java/com/example/piled_client/yellow.java
@@ -0,0 +1,56 @@
+package com.example.piled_client;
+
+import java.net.URL;
+import android.util.Log;
+import android.os.AsyncTask;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
+class yellow extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... params) {
+        StringBuilder sb = null;
+        BufferedReader reader = null;
+        String serverResponse = null;
+
+        try {
+            URL url = new URL("http://192.168.28.43/yellow.php");
+            HttpURLConnection conn_yellow = (HttpURLConnection) url.openConnection();
+            conn_yellow.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn_yellow.setConnectTimeout(5000);
+            conn_yellow.setRequestMethod("GET");
+            conn_yellow.connect();
+
+            int conStatusCode = conn_yellow.getResponseCode();
+            if(conStatusCode == 200){
+                sb = new StringBuilder();
+                reader = new BufferedReader(new InputStreamReader(conn_yellow.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null){
+                    sb.append(line).append("\n");
+                }
+            }
+            conn_yellow.disconnect();
+            if (sb != null){
+                serverResponse = sb.toString();
+            }
+
+        }
+        catch (Exception ex) {
+            Log.e("Error:", " " + ex);
+        }
+        finally {
+            if (reader != null){
+                try {
+                    reader.close();
+                }
+                catch (Exception ex){
+                    Log.e("Error:", " " + ex);
+                }
+            }
+        }
+        return serverResponse;
+    }
+}
