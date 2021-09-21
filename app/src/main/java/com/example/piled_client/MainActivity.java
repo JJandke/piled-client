bb30c011a80a743a2e632e6e9a6f7732cd1e39d9
Johannes <johannesjandke@gmail.com>
2021-09-21 18:59:17 +0200

    calling red to purple via xml

--- app/src/main/java/com/example/piled_client/MainActivity.java
+++ app/src/main/java/com/example/piled_client/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         sw_onoff = findViewById(R.id.sw_onoff);
     }
 
+
+    // switches
     public void  onOff(@Nullable View view){
         if (sw_onoff.isChecked()){
             new power_on().execute();
@@ -27,5 +29,31 @@ public class MainActivity extends AppCompatActivity {
         else{
             new power_off().execute();
         }
+    }
+
+
+    // buttons
+    public void  red(@Nullable View view){
+        new red().execute();
+    }
+
+    public void  green(@Nullable View view){
+        new green().execute();
+    }
+
+    public void  blue(@Nullable View view){
+        new blue().execute();
+    }
+
+    public void  orange(@Nullable View view){
+        new orange().execute();
+    }
+
+    public void  turquoise(@Nullable View view){
+        new turquoise().execute();
+    }
+
+    public void purple(@Nullable View view){
+        new orange().execute();
     }
 }
